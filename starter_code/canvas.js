c7faf28a298f95c0847d5bb1d0a65e7a706f86c5
Ariel Fonticiella <arielfonticiella@Ariels-MBP.attlocal.net>
2017-10-29 23:40:54 -0400

    Initial Commit - simply have a start page with functioning Start button.

---
+++ starter_code/canvas.js
@@ -0,0 +1,10 @@
+// var board = document.getElementById('game-board');
+// var canvas = board.getElementById('raceCar');
+// canvas.width = window.innerWidth - 30;
+//
+// var c = canvas.getContext('2d');
+//
+//   c.beginPath();
+//   c.moveTo(100, 300);
+//   c.lineTo(300, 300);
+//   c.stroke();
--- starter_code/script.js
+++ starter_code/script.js
@@ -3,7 +3,18 @@ window.onload = function() {
     startGame();
   };
 
-  function startGame() {
 
+
+  function startGame() {
+    $("#start-button").click(function () {
+        $("#SplashScreen").hide();
+        $("#game-board").show();
+    });
   }
-};
+}; //<--- window.onload function close.
+
+var canvas = document.getElementById('game-board');
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+var c = canvas.getContext('2d');
